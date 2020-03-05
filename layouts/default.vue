7082c4feefccbf7c400937d087557fd856b1ebbd
Dheeraj <dj.sethi09@gmail.com>
2020-03-05 21:18:23 +0530

    Initial commit with non solid app

---
+++ layouts/default.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-app>
+    <v-app-bar :clipped-left="clipped" fixed app>
+      <v-toolbar-title v-text="title" />
+    </v-app-bar>
+    <v-content>
+      <v-container>
+        <nuxt />
+      </v-container>
+    </v-content>
+    <v-footer :fixed="fixed" app>
+      <span>&copy; {{ new Date().getFullYear() }}</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      fixed: false,
+      items: [
+        {
+          icon: "mdi-apps",
+          title: "Welcome",
+          to: "/"
+        }
+      ],
+      title: "SOLID in VueJs by TechCharades"
+    };
+  }
+};
+</script>
