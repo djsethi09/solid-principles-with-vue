10e063e02fdfefcc6330b2c11726f2acbda711d5
djsethi09 <dj.sethi09@gmail.com>
2020-03-06 15:29:51 +0530

    ISP extracted the carousel item to pass on just the required props instead of all. just like ModelView in mvc.net

---
+++ components/CarouselItem.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-hover>
+    <template v-slot:default="{ hover }">
+      <v-card class="mx-auto" max-width="344">
+        <v-img :src="item.url"></v-img>
+        <v-card-text>
+          <h2 class="title primary--text">{{item.title}}</h2>
+        </v-card-text>
+        <v-fade-transition>
+          <v-overlay v-if="hover" absolute color="#036358"></v-overlay>
+        </v-fade-transition>
+      </v-card>
+    </template>
+  </v-hover>
+</template>
+<script lang="ts">
+import { Vue, Component, Prop } from "vue-property-decorator";
+import ICarouselItem from "@/types/CarouselItem";
+@Component
+export default class CarouselItem extends Vue {
+  @Prop({ type: Object, required: true }) readonly item!: ICarouselItem;
+}
+</script>
--- components/PhotosGrid.vue
+++ components/PhotosGrid.vue
@@ -1,24 +1,27 @@
 <template>
   <v-carousel hide-delimiters cycle height="400" hide-delimiter-background show-arrows-on-hover>
     <v-carousel-item v-for="(slide, i) in photos" :key="i">
-      <v-hover>
-        <template v-slot:default="{ hover }">
-          <v-card class="mx-auto" max-width="344">
-            <v-img :src="slide.url"></v-img>
-            <v-card-text>
-              <h2 class="title primary--text">{{slide.title}}</h2>
-            </v-card-text>
-            <v-fade-transition>
-              <v-overlay v-if="hover" absolute color="#036358"></v-overlay>
-            </v-fade-transition>
-          </v-card>
-        </template>
-      </v-hover>
+      <CarouselItem :item="{title:slide.title,url:slide.url}" />
     </v-carousel-item>
   </v-carousel>
 </template>
-<script>
-export default {
-  props: { photos: {}, headers: {} }
-};
+<script lang="ts">
+import { Vue, Component, Prop } from "vue-property-decorator";
+import { IPhoto } from "~/types/Photo";
+import CarouselItem from "@/components/CarouselItem.vue";
+@Component({ components: { CarouselItem } })
+export default class PhotosGrid extends Vue {
+  @Prop({
+    default: () => {
+      [];
+    }
+  })
+  photos!: IPhoto[];
+  @Prop({
+    default: () => {
+      [];
+    }
+  })
+  headers!: any[];
+}
 </script>
